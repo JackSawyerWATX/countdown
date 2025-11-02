9153e2c0f1fa237721c1382fbec626a044a877fb
Jack Sawyer <contact@jonathanfausset.com>
2025-11-02 00:50:11 -0500

    Added if/else

--- countdown.py
+++ countdown.py
@@ -3,7 +3,10 @@ import time
 seconds = int(input("Enter seconds for countdown: "))
 
 for i in range(seconds, -1, -1):
-  print(f"Time left: {i} seconds",
+  if i == -1:
+    print("Finished")
+  else:
+    print(f" Time left: {i} seconds",
     end='\r')
   time.sleep(1)
-print(" Finished")
+print(" Time's up")
